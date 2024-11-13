eb9548b4189e4bf231a251e2e19a9432d2b2ad8b
Benfranklin m s <bfranklinms@gmail.com>
2024-11-13 19:17:02 +0530

    updated

---
+++ File handling/Print All Lines That Contain Numbers/main.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+public class main{
+    public static void main(String args[]){
+        File file1 = new File("source.txt");
+        File file2 = new File("destination.txt");
+
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+
+        try{
+            br = new BufferedReader(new FileReader(file1));
+            bw = new BufferedWriter(new FileWriter(file2));
+
+            String line;
+
+            while((line = br.readLine()) != null){
+                if(line.matches(".*\\d.*")){
+                    bw.write(line);
+                    bw.newLine();
+                }
+            }
+        }
+
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
+        try{
+            br.close();
+            bw.close();
+        }
+
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
