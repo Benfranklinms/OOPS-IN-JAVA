e8065fa39604e32c7e8dc011ffb1f41cb0097683
Benfranklin m s <bfranklinms@gmail.com>
2024-11-13 10:14:17 +0530

    updated

---
+++ Reverse Words in a letter/main.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+public class main{
+    public static void main(String args[]){
+        File file1 = new File("source.txt");
+        File file2 = new File("destination.txt");
+
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+
+        try{
+            br = new BufferedReader(new FileReader(file1));
+            bw = new BufferedWriter(new FileWriter(file2));
+
+            String line;
+
+            while((line = br.readLine()) != null){
+                String[] words = line.split(" ");
+                int i;
+                for(i = 0; i < words.length; i++){
+                    String reversed = new StringBuilder(words[i]).reverse().toString();
+                    bw.write(reversed + " ");
+                }
+                bw.newLine();
+            }
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
+        try{
+            br.close();
+            bw.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
