523290f2ffcef56454c8c454ac3aa13340662852
Benfranklin m s <bfranklinms@gmail.com>
2024-11-14 23:45:15 +0530

    updated

--- Java csa/Qn2/main.java
+++ Java csa/Qn2/main.java
@@ -8,6 +8,81 @@ consumed in 1000km and distance travelled in 90 minutes. Write the output to
 import java.io.*;
 import java.util.*;
 
+
 abstract class vehicle{
-    abstract void calcFuelUsage()
+    public abstract double calcFuelUsage(double distance);
+    static int wheels;
+        static int year;
+            
+                public vehicle(int wheels, int year){
+                    this.wheels = wheels;
+                    this.year = year;
+                }
+                void display(){
+                    System.out.println("Name of the class : "+ this.getClass().getName());
+                    System.out.println("Wheels : " +wheels);
+                    System.out.println("Year : " +year);
+                }
+            }
+            
+            
+            
+            class car extends vehicle{
+                final static double maxSpeed;
+                final static double mileage;
+                
+                    public car(int wheels, int year, double maxSpeed, double mileage){
+                        super(wheels, year);
+                        this.maxSpeed = maxSpeed;
+                        this.mileage = mileage;
+                    }
+                        public static double calcFuelUsage(double distance){
+                                    return distance / mileage;
+                            }
+                    
+                            public static double distanceTravelled(double time){
+                                        return time / maxSpeed;
+                                    }
+                            
+                            }
+                            
+                            class motor extends vehicle{
+                                final double maxSpeed;
+                                final double mileage;
+                                public motor(int wheels, int year, double maxSpeed, double mileage){
+                                super(wheels, year);
+                                this.maxSpeed = maxSpeed;
+                                this.mileage = mileage;
+                                }
+                                public double calcFuelUsage(double distance){
+                                    return distance / mileage;
+                                }
+                            
+                                public double distanceTravelled(double time){
+                                    return time / maxSpeed;
+                                }
+                            }
+                            public class main{
+                                public static void main(String[] args) {
+                                    car c = new car(4, 2015, 200, 10);
+                                    motor m = new motor(2, 2010, 150, 15);
+                            
+                                    double distance = 1000;
+                                    double time = 10;
+                            
+                                    File file1 = new File("source.txt");
+                            
+                                    BufferedWriter bw = null;
+                            
+                                    try{
+                                        bw = new BufferedWriter(new FileWriter(file1));
+                                        bw.write(car.wheels);
+                                    bw.write(car.year);
+                            bw.write((int) car.calcFuelUsage(distance));
+                    bw.write((int) car.distanceTravelled(time));
+        }
+        catch(IOException e){
+            System.out.println("Error");
+        }
+    }
 }
