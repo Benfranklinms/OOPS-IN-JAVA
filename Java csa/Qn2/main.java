aa731b6115120ee5f5ad943e06f82cb5c221f768
Benfranklin m s <bfranklinms@gmail.com>
2024-11-14 22:28:53 +0530

    added

--- Java csa/Qn2/main.java
+++ Java csa/Qn2/main.java
@@ -1,5 +1,13 @@
-package Java csa.Qn2;
+/* Create an abstract class named vehicle having abstract method calcFuelUsage, a
+method display that gives name of the class and some variables wheels, year, etc.
+Two subclasses Car and Motorcycle extends Vehicle having final fields maxspeed
+and mileage. Values of maxspeed and mileage are given for each subclass. Find fuel
+consumed in 1000km and distance travelled in 90 minutes. Write the output to
+"vehiclecompare.txt" which is taken as command line argument. */
 
-public class main {
-    
+import java.io.*;
+import java.util.*;
+
+abstract class vehicle{
+    abstract void calcFuelUsage()
 }
