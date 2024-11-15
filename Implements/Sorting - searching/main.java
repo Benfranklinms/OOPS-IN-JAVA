72ab47c2594c3c6615edd6792802faa62b5fe2b6
Benfranklin m s <bfranklinms@gmail.com>
2024-11-15 20:08:12 +0530

    updated

---
+++ Implements/Sorting - searching/main.java
@@ -0,0 +1,4 @@
+/* Create an interface array with method such as sort, insert, delete and binary search.
+Create a class singlearray to inherit the interface and override all the member function.
+Implement java program for this. */
+
